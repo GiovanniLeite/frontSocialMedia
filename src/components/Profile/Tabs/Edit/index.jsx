373edb45e9f3cabc3a5ab88f2808812ed03f6f5e
Giovanni Leite <giovanni.m.leite@outlook.com>
2024-05-04 15:36:10 -0300

    Update style of Edit section in Profile page

--- src/components/Profile/Tabs/Edit/index.jsx
+++ src/components/Profile/Tabs/Edit/index.jsx
@@ -17,7 +17,6 @@ export default function Edit() {
     errors: { update: apiErrors },
   } = useSelector((state) => state.auth);
   const coverPath = '1711735862193_13757.jpg';
-  // const coverPath = 'http://localhost:3001/images/posts/1711735862193_13757.jpg';
 
   const handleFormSubmit = (values) => {
     console.log('foi');
@@ -25,37 +24,174 @@ export default function Edit() {
 
   return (
     <Container>
-      <div>
-        <h4>Capa</h4>
-        <div className="coverDiv">
-          <img src={`${API_URL}images/posts/${coverPath}`} alt={coverPath} title={coverPath} />
+      <div className="formGroup">
+        {/* INFO */}
+        <div>
+          <h4>Informações</h4>
+          <Formik onSubmit={handleFormSubmit} initialValues={user} validationSchema={formValidation.schema}>
+            {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <div className="doubleText">
+                  <TextField
+                    label="Nome"
+                    id="firstName"
+                    name="firstName"
+                    value={values.firstName}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={touched.firstName && errors.firstName}
+                    disabled={isLoading}
+                  />
+                  <TextField
+                    label="Sobrenome"
+                    id="lastName"
+                    name="lastName"
+                    value={values.lastName}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={touched.lastName && errors.lastName}
+                    disabled={isLoading}
+                  />
+                </div>
+                <TextField
+                  label="Cidade, Estado"
+                  id="location"
+                  name="location"
+                  value={values.location}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  error={touched.location && errors.location}
+                  disabled={isLoading}
+                />
+                <TextField
+                  label="Ocupação"
+                  id="occupation"
+                  name="occupation"
+                  value={values.occupation}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  error={touched.occupation && errors.occupation}
+                  disabled={isLoading}
+                />
+                <TextField
+                  label="Twitter/X"
+                  mask={['twitter.com/', ...Array(50).fill(/[a-zA-Z0-9-]/)]}
+                  id="twitter"
+                  name="twitter"
+                  value={values.twitter}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  error={touched.twitter && errors.twitter}
+                  disabled={isLoading}
+                />
+                <TextField
+                  label="Linkedin"
+                  mask={['linkedin.com/in/', ...Array(50).fill(/[a-zA-Z0-9-]/)]}
+                  id="linkedin"
+                  name="linkedin"
+                  value={values.linkedin}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  error={touched.linkedin && errors.linkedin}
+                  disabled={isLoading}
+                />
+
+                <button
+                  type={isLoading ? 'button' : 'submit'}
+                  className={isLoading ? 'buttonLoading' : ''}
+                  title="Criar Perfil"
+                >
+                  {isLoading ? <Loading /> : 'Salvar'}
+                </button>
+
+                {apiErrors.map((error, index) => (
+                  <p className="error" key={index}>
+                    {error}
+                  </p>
+                ))}
+              </form>
+            )}
+          </Formik>
+        </div>
+        {/* EMAIL */}
+        <div>
+          <h4>Email</h4>
+          <Formik onSubmit={handleFormSubmit} initialValues={user} validationSchema={formValidation.schema}>
+            {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  label="Email"
+                  id="email"
+                  name="email"
+                  value={values.email}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  error={touched.email && errors.email}
+                  disabled={isLoading}
+                />
+
+                <button
+                  type={isLoading ? 'button' : 'submit'}
+                  className={isLoading ? 'buttonLoading' : ''}
+                  title="Criar Perfil"
+                >
+                  {isLoading ? <Loading /> : 'Salvar'}
+                </button>
+
+                {apiErrors.map((error, index) => (
+                  <p className="error" key={index}>
+                    {error}
+                  </p>
+                ))}
+              </form>
+            )}
+          </Formik>
+        </div>
+        {/* PASSWORD */}
+        <div>
+          <h4>Senha</h4>
           <Formik
             onSubmit={handleFormSubmit}
-            initialValues={{ coverPath: coverPath }}
+            initialValues={formValidation.initialValues}
             validationSchema={formValidation.schema}
           >
-            {({ values, handleBlur, handleChange, handleSubmit }) => (
+            {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
               <form onSubmit={handleSubmit}>
-                <TextField
-                  label="Arquivo"
-                  id="coverPath"
-                  name="coverPath"
-                  value={values.coverPath}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  disabled
-                />
+                <div className="doubleText">
+                  <TextField
+                    type="password"
+                    label="Senha"
+                    id="password"
+                    name="password"
+                    value={values.password}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={touched.password && errors.password}
+                    disabled={isLoading}
+                  />
+                  <TextField
+                    type="password"
+                    label="Confirmar"
+                    id="passwordConfirm"
+                    name="passwordConfirm"
+                    value={values.passwordConfirm}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={touched.passwordConfirm && errors.passwordConfirm}
+                    disabled={isLoading}
+                  />
+                </div>
 
                 <button
                   type={isLoading ? 'button' : 'submit'}
                   className={isLoading ? 'buttonLoading' : ''}
-                  title="Procurar"
+                  title="Criar Perfil"
                 >
-                  {isLoading ? <Loading /> : 'Procurar'}
+                  {isLoading ? <Loading /> : 'Salvar'}
                 </button>
 
                 {apiErrors.map((error, index) => (
-                  <p className="errors" key={index}>
+                  <p className="error" key={index}>
                     {error}
                   </p>
                 ))}
@@ -64,200 +200,83 @@ export default function Edit() {
           </Formik>
         </div>
       </div>
+      <div className="formGroup">
+        {/* COVER */}
+        <div className="coverContainer">
+          <h4>Capa</h4>
+          <div>
+            <img src={`${API_URL}images/posts/${coverPath}`} alt={coverPath} title={coverPath} />
+            <Formik
+              onSubmit={handleFormSubmit}
+              initialValues={{ coverPath: coverPath }}
+              validationSchema={formValidation.schema}
+            >
+              {({ values, handleBlur, handleChange, handleSubmit }) => (
+                <form onSubmit={handleSubmit}>
+                  <TextField
+                    label="Arquivo"
+                    id="coverPath"
+                    name="coverPath"
+                    value={values.coverPath}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    disabled
+                  />
 
-      <div>
-        <h4>Foto</h4>
-        <div className="pictureDiv">
-          <UserImage image={user.picturePath} userName={user.firstName} size={80} />
-          <Formik onSubmit={handleFormSubmit} initialValues={user} validationSchema={formValidation.schema}>
-            {({ values, handleBlur, handleChange, handleSubmit }) => (
-              <form onSubmit={handleSubmit}>
-                <TextField
-                  label="Arquivo"
-                  id="picturePath"
-                  name="picturePath"
-                  value={values.picturePath}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  disabled
-                />
+                  <button
+                    type={isLoading ? 'button' : 'submit'}
+                    className={isLoading ? 'buttonLoading' : ''}
+                    title="Procurar"
+                  >
+                    {isLoading ? <Loading /> : 'Procurar'}
+                  </button>
+                </form>
+              )}
+            </Formik>
+          </div>
 
-                <button
-                  type={isLoading ? 'button' : 'submit'}
-                  className={isLoading ? 'buttonLoading' : ''}
-                  title="Procurar"
-                >
-                  {isLoading ? <Loading /> : 'Procurar'}
-                </button>
-
-                {apiErrors.map((error, index) => (
-                  <p className="errors" key={index}>
-                    {error}
-                  </p>
-                ))}
-              </form>
-            )}
-          </Formik>
+          {apiErrors.map((error, index) => (
+            <p className="error errorMarginStyle" key={index}>
+              {error}
+            </p>
+          ))}
         </div>
-      </div>
+        {/* PICTURE */}
+        <div className="pictureContainer">
+          <h4>Foto</h4>
+          <div>
+            <UserImage image={user.picturePath} userName={user.firstName} size={80} />
+            <Formik onSubmit={handleFormSubmit} initialValues={user} validationSchema={formValidation.schema}>
+              {({ values, handleBlur, handleChange, handleSubmit }) => (
+                <form onSubmit={handleSubmit}>
+                  <TextField
+                    label="Arquivo"
+                    id="picturePath"
+                    name="picturePath"
+                    value={values.picturePath}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    disabled
+                  />
 
-      <div>
-        <h4>Informações</h4>
-        <Formik
-          onSubmit={handleFormSubmit}
-          initialValues={formValidation.initialValues}
-          validationSchema={formValidation.schema}
-        >
-          {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
-            <form onSubmit={handleSubmit}>
-              <div className="doubleText">
-                <TextField
-                  label="Nome"
-                  id="firstName"
-                  name="firstName"
-                  value={values.firstName}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  error={touched.firstName && errors.firstName}
-                  disabled={isLoading}
-                />
-                <TextField
-                  label="Sobrenome"
-                  id="lastName"
-                  name="lastName"
-                  value={values.lastName}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  error={touched.lastName && errors.lastName}
-                  disabled={isLoading}
-                />
-              </div>
-              <TextField
-                label="Cidade, Estado"
-                id="location"
-                name="location"
-                value={values.location}
-                onBlur={handleBlur}
-                onChange={handleChange}
-                error={touched.location && errors.location}
-                disabled={isLoading}
-              />
-              <TextField
-                label="Ocupação"
-                id="occupation"
-                name="occupation"
-                value={values.occupation}
-                onBlur={handleBlur}
-                onChange={handleChange}
-                error={touched.occupation && errors.occupation}
-                disabled={isLoading}
-              />
-              <TextField
-                label="Twitter/X"
-                mask={['twitter.com/', ...Array(50).fill(/[a-zA-Z0-9-]/)]}
-                id="twitter"
-                name="twitter"
-                value={values.twitter}
-                onBlur={handleBlur}
-                onChange={handleChange}
-                error={touched.twitter && errors.twitter}
-                disabled={isLoading}
-              />
-              <TextField
-                label="Linkedin"
-                mask={['linkedin.com/in/', ...Array(50).fill(/[a-zA-Z0-9-]/)]}
-                id="linkedin"
-                name="linkedin"
-                value={values.linkedin}
-                onBlur={handleBlur}
-                onChange={handleChange}
-                error={touched.linkedin && errors.linkedin}
-                disabled={isLoading}
-              />
-              <button
-                type={isLoading ? 'button' : 'submit'}
-                className={isLoading ? 'buttonLoading' : ''}
-                title="Criar Perfil"
-              >
-                {isLoading ? <Loading /> : 'Salvar'}
-              </button>
-            </form>
-          )}
-        </Formik>
-      </div>
+                  <button
+                    type={isLoading ? 'button' : 'submit'}
+                    className={isLoading ? 'buttonLoading' : ''}
+                    title="Procurar"
+                  >
+                    {isLoading ? <Loading /> : 'Procurar'}
+                  </button>
+                </form>
+              )}
+            </Formik>
+          </div>
 
-      <div>
-        <h4>Email</h4>
-        <Formik
-          onSubmit={handleFormSubmit}
-          initialValues={formValidation.initialValues}
-          validationSchema={formValidation.schema}
-        >
-          {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
-            <form onSubmit={handleSubmit}>
-              <TextField
-                label="Email"
-                id="email"
-                name="email"
-                value={values.email}
-                onBlur={handleBlur}
-                onChange={handleChange}
-                error={touched.email && errors.email}
-                disabled={isLoading}
-              />
-              <button
-                type={isLoading ? 'button' : 'submit'}
-                className={isLoading ? 'buttonLoading' : ''}
-                title="Criar Perfil"
-              >
-                {isLoading ? <Loading /> : 'Salvar'}
-              </button>
-            </form>
-          )}
-        </Formik>
-      </div>
-
-      <div>
-        <h4>Senha</h4>
-        <Formik
-          onSubmit={handleFormSubmit}
-          initialValues={formValidation.initialValues}
-          validationSchema={formValidation.schema}
-        >
-          {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
-            <form onSubmit={handleSubmit}>
-              <TextField
-                type="password"
-                label="Senha"
-                id="password"
-                name="password"
-                value={values.password}
-                onBlur={handleBlur}
-                onChange={handleChange}
-                error={touched.password && errors.password}
-                disabled={isLoading}
-              />
-              <TextField
-                type="password"
-                label="Confirmar senha"
-                id="passwordConfirm"
-                name="passwordConfirm"
-                value={values.passwordConfirm}
-                onBlur={handleBlur}
-                onChange={handleChange}
-                error={touched.passwordConfirm && errors.passwordConfirm}
-                disabled={isLoading}
-              />
-              <button
-                type={isLoading ? 'button' : 'submit'}
-                className={isLoading ? 'buttonLoading' : ''}
-                title="Criar Perfil"
-              >
-                {isLoading ? <Loading /> : 'Salvar'}
-              </button>
-            </form>
-          )}
-        </Formik>
+          {apiErrors.map((error, index) => (
+            <p className="error errorMarginStyle" key={index}>
+              {error}
+            </p>
+          ))}
+        </div>
       </div>
     </Container>
   );
--- src/components/Profile/Tabs/Edit/styles.js
+++ src/components/Profile/Tabs/Edit/styles.js
@@ -2,60 +2,101 @@ import styled, { css } from 'styled-components';
 
 export const Container = styled.section`
   ${({ theme }) => css`
-    padding: ${theme.spacings.medium};
     background-color: ${theme.colors.background.alt};
+    padding: ${theme.spacings.medium};
     border-radius: 0.7rem;
     display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
     gap: ${theme.spacings.medium};
 
-    h4 {
-      color: ${theme.colors.neutral.dark};
-      font-weight: 500;
-      margin-bottom: ${theme.spacings.small};
+    @media screen and (max-width: 720px) {
+      grid-template-columns: 100%;
     }
 
-    button {
-      background-color: ${theme.colors.primary.main};
-      padding: 1.3rem;
-      border: none;
-      border-radius: 0.5rem;
-      color: #fff;
+    .formGroup {
+      display: grid;
+      gap: ${theme.spacings.medium};
+      align-self: start;
 
-      &:hover:not(.buttonLoading) {
-        cursor: pointer;
-        background-color: ${theme.colors.primary.mediumMain};
+      @media screen and (max-width: 720px) {
+        &:nth-child(2) {
+          order: -1;
+        }
       }
 
-      &.buttonLoading {
-        padding: 0.3rem;
-      }
-    }
-
-    .errors {
-      color: ${theme.colors.error};
-      padding: ${theme.spacings.extraSmall} 0 0 ${theme.spacings.extraSmall};
-      margin-top: -${theme.spacings.small};
-    }
-
-    .coverDiv {
-      img {
-        border-radius: 0.7rem;
-        width: 100%;
+      h4 {
+        color: ${theme.colors.neutral.dark};
+        font-weight: 500;
         margin-bottom: ${theme.spacings.small};
       }
 
       form {
-      }
-    }
-
-    .pictureDiv {
-      display: flex;
-      align-items: center;
-
-      form {
-        display: flex;
-        align-items: center;
+        display: grid;
         gap: ${theme.spacings.small};
+
+        .doubleText {
+          display: grid;
+          gap: ${theme.spacings.small};
+          grid-template-columns: repeat(2, minmax(0, 1fr));
+        }
+      }
+
+      button {
+        background-color: ${theme.colors.primary.main};
+        padding: 1.3rem;
+        border: none;
+        border-radius: 0.5rem;
+        color: #fff;
+
+        &:hover:not(.buttonLoading) {
+          cursor: pointer;
+          background-color: ${theme.colors.primary.mediumMain};
+        }
+
+        &.buttonLoading {
+          padding: 0.3rem;
+        }
+      }
+
+      .error {
+        color: ${theme.colors.error};
+        text-align: center;
+
+        &.errorMarginStyle {
+          margin: ${theme.spacings.small} 0;
+        }
+      }
+
+      .coverContainer {
+        img {
+          width: 100%;
+          border-radius: 0.7rem;
+          margin-bottom: ${theme.spacings.small};
+        }
+
+        form {
+          grid-template-columns: auto 7.8rem;
+        }
+      }
+
+      .pictureContainer {
+        & > div {
+          display: grid;
+          grid-template-columns: 9rem auto;
+
+          @media screen and (max-width: 360px) {
+            grid-template-columns: 100%;
+
+            img {
+              margin: 0 auto ${theme.spacings.small};
+            }
+          }
+
+          form {
+            align-items: center;
+            grid-template-columns: auto 7.8rem;
+          }
+        }
       }
     }
   `}
